48637b9adbd576b982c3d325c3817991f70be0b2
ihsnack <ihsnack@fastmail.com>
2025-08-29 13:41:23 +0200

    upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// SSH är ett nätversprotokoll som möjliggör säker kommunikation mellan två datorer över ett osäkert nätverk.
+
