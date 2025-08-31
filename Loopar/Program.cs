56c5ae33dbc1aff4a4c2ce0be8b314174645591a
ihsnack <ihsnack@fastmail.com>
2025-08-31 21:37:23 +0200

    done

--- Loopar/Program.cs
+++ Loopar/Program.cs
@@ -16,3 +16,135 @@
 
 // dhttps://www.youtube.com/watch?v=MKjIhNYVMjM 8:10t
 
+//List<string> names = ["Hans", "Tommy", "Joakim"];
+
+//for (int i = 0; i < names.Count; i++)
+//{
+//    Console.WriteLine(names[i]);
+//}
+
+//List<string> names = ["Hans", "Tommy", "Joakim"];
+
+//foreach (var name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+//foreach (string name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+//names.ForEach(name => {
+//    Console.WriteLine(name);
+//});
+
+//string[] names = ["Hans", "Tommy", "Joakim"];
+
+//foreach (var name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+//foreach (string name in names)
+//{
+//    Console.WriteLine(name);
+//}
+// Går ej med array
+//names.ForEach(name => {
+//    Console.WriteLine(name);
+//});
+
+//IEnumerable<string> names = ["Hans", "Tommy", "Joakim"];
+
+//foreach (var name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+//foreach (string name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+// Går ej med IEnumerable
+//names.ForEach(name => {
+//    Console.WriteLine(name);
+//});
+
+//int number = 0;
+
+//while(true)
+//{
+//    number++;
+//    Console.WriteLine(number);
+//}
+
+//do
+//{
+
+//}
+//while (true);
+
+//int number = 0;
+
+//while(number < 5)
+//{
+//    Console.Clear();
+//    Console.WriteLine($"number = {number}");
+
+//    for (int i = 0; i < 50000; i++ )
+//    {
+//        Console.WriteLine($"i = {i}");
+//    }
+
+//    if(number == 3)
+//    {
+//        break;
+//    }
+
+//    number++;
+
+//}
+
+//Console.WriteLine("All done");
+
+//Console.Clear();
+
+//string answer = "";
+
+//while(answer != "q")
+//{
+//    Console.WriteLine("MENU OPTIONS");
+//    Console.WriteLine("1. OPTION 1");
+//    Console.WriteLine("2. OPTION 2");
+//    Console.WriteLine("3. OPTION 3");
+//    Console.WriteLine("Q. QUIT");
+
+//    answer = Console.ReadLine()!;
+//}
+
+//Console.Clear();
+
+//string answer = "";
+
+//do
+//{
+//    Console.WriteLine("MENU OPTIONS");
+//    Console.WriteLine("1. OPTION 1");
+//    Console.WriteLine("2. OPTION 2");
+//    Console.WriteLine("3. OPTION 3");
+//    Console.WriteLine("Q. QUIT");
+
+//    answer = Console.ReadLine()!;
+//}
+//while (answer != "q");
+//Console.Clear();
+
+//bool isConnected = false;
+
+//while (!isConnected)
+//{
+//    Console.WriteLine("Connection....");
+//    isConnected = true;
+//}
