eded34be524a258ab75aeb56fb9ec2e3a2b65997
ihsnack <ihsnack@fastmail.com>
2025-08-31 09:50:49 +0200

    dsa

---
+++ Loopar/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//string[] names = ["Hans", "Tommy", "Joakim"];
+
+//for (int i = 0; i < names.Length; i++)
+//{
+//    Console.WriteLine(names[i]);
+//}
+
+
+// dhttps://www.youtube.com/watch?v=MKjIhNYVMjM 8:10
